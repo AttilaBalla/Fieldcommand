b370a2e1c123f0b294f9a397f38843efd2822201
attilaballa <xattus@gmail.com>
2018-04-02 13:01:55 +0200

    converted project to use spring framework

---
+++ src/main/java/personal/fieldcommand/Controller/ViewController.java
@@ -0,0 +1,33 @@
+package personal.fieldcommand.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String renderIndex() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String renderAbout() {
+        return "about";
+    }
+
+    @RequestMapping(value = "/releases", method = RequestMethod.GET)
+    public String renderReleases() {
+        return "releases";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String renderAdmin() {
+        return "administration";
+    }
+}
