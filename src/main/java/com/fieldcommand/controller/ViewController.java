1947f434e5eee8d2899805e14fab1f176890bc5d
attilaballa <xattus@gmail.com>
2018-04-04 15:15:01 +0200

    html template dir strucutre added

--- src/main/java/com/fieldcommand/controller/ViewController.java
+++ src/main/java/com/fieldcommand/controller/ViewController.java
@@ -35,7 +35,7 @@ public class ViewController {
             return "administration";
         }
         else {
-            return subPage;
+            return "/fragments/subpages/" + subPage;
         }
     }
     //temporary
