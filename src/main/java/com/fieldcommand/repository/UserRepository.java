c7600850f409074be7cd62101292c77efba48e16
AttilaBalla <xattus@gmail.com>
2018-04-10 22:25:01 +0200

    added app property profiles

--- src/main/java/com/fieldcommand/repository/UserRepository.java
+++ src/main/java/com/fieldcommand/repository/UserRepository.java
@@ -3,11 +3,7 @@ package com.fieldcommand.repository;
 import com.fieldcommand.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserWriteRepository extends CrudRepository<User, Long> {
 
     User findUserByEmail(String email);
-
-    List<User> findAll();
 }
