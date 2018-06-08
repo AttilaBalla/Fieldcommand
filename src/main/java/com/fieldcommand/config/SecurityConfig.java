a2084b71bd29a623fb2d8f5851b9d110f4213dbc
AttilaBalla <xattus@gmail.com>
2018-06-08 07:37:27 +0200

    user access WIP

--- src/main/java/com/fieldcommand/config/SecurityConfig.java
+++ src/main/java/com/fieldcommand/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.fieldcommand.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -74,6 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/api/swrStatus")
                 .permitAll()
+                //.antMatchers("/api/admin/**").hasAuthority("ADMIN")
                 .anyRequest()
                 .authenticated();
 
