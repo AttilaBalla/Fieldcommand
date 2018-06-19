ae8d999ae3a5fd229d3397ef04030c10acaafd73
sevendiko <sevendiko@gmail.com>
2018-06-19 11:09:05 +0200

    user ban finally works fine

--- src/main/java/com/fieldcommand/user/UserPrincipal.java
+++ src/main/java/com/fieldcommand/user/UserPrincipal.java
@@ -2,7 +2,8 @@ package com.fieldcommand.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fieldcommand.role.RoleType;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,10 +13,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class UserPrincipal implements UserDetails{
 
-    @Autowired
-    UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private Long id;
 
@@ -97,7 +98,7 @@ public class UserPrincipal implements UserDetails{
 
     @Override
     public boolean isEnabled() {
-        return userRepository.findUserById(id).getRole().getRoleType() != RoleType.ROLE_DISABLED;
+        return !this.roleType.equals(String.valueOf(RoleType.ROLE_DISABLED));
     }
 
     public Long getId() {
