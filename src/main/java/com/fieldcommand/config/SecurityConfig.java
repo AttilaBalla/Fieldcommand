81e47fe594876daf8ec6d05191e44bedfcaa27aa
sevendiko <sevendiko@gmail.com>
2018-06-20 11:33:17 +0200

    Modified security config

--- src/main/java/com/fieldcommand/config/SecurityConfig.java
+++ src/main/java/com/fieldcommand/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/swrStatus").permitAll()
-                .antMatchers("/internal/request").permitAll()
+                .antMatchers("/api/user/ir/**").permitAll()
                 .antMatchers("/api/admin/**").hasAuthority("AUTHORITY_ADMIN")
                 .anyRequest()
                 .authenticated();
