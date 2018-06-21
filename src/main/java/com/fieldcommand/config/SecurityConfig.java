c50bf91e8c7207baa8f85c7158cc57a8065abae7
AttilaBalla <xattus@gmail.com>
2018-06-21 22:57:50 +0200

    getNewsPosts access added

--- src/main/java/com/fieldcommand/config/SecurityConfig.java
+++ src/main/java/com/fieldcommand/config/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/swrStatus").permitAll()
+                .antMatchers("/api/getNewsPosts").permitAll()
                 .antMatchers("/api/dev/addNewsPost").hasAuthority("AUTHORITY_NEWS_CREATE")
                 .antMatchers("/api/admin/**").hasAuthority("AUTHORITY_ADMIN")
                 .anyRequest()
